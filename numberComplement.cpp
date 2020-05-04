d3814f35498033c5db8fa44908fa12df90ac1ff4
rohit236c <rohit236c@gmail.com>
2020-05-04 17:26:58 +0530

    lt-number-complement
    Signed-off-by: rohit236c <rohit236c@gmail.com>

---
+++ numberComplement.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+int findComplement(int num) {
+	int ans = 0;
+	cout << num << endl;
+	int occ = 0;
+	for(int bit = 30; bit >= 0; bit--) {
+		if((num & (1 << bit))) {
+			occ = 1; 
+		} else if(occ == 1) {
+			ans = ans | (1<<bit);
+		}
+	}
+	return ans;
+
+}
+void solve() {
+	cout << findComplement(2);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	solve();
+
+	return 0;
+}
